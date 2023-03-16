6476f1e7116492d0a230f6de47a02c7e622f8e59
Chiwang Lee <122594223+noxknow@users.noreply.github.com>
2023-03-16 15:39:55 +0900

    Create 2644 촌수 계산.py

---
+++ 01. 그래프 탐색/2644 촌수 계산.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def bfs(node):
+    q = deque([(node)])
+    while q:
+        node = q.popleft()
+        for n in graph[node]:
+            if visited[n] == 0:
+                visited[n] = visited[node]+1
+                q.append(n)
+            
+n = int(input())
+graph = [[] for _ in range(n+1)]
+s, e = map(int, input().split())
+for _ in range(int(input())):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+visited = [0]*(n+1)
+bfs(s)
+print(visited[e] if visited[e] > 0 else -1)
