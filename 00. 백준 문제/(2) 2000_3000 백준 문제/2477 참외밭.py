426cbbfc3d1f06e278fc4560ea349920822b7e41
Chiwang Lee <122594223+noxknow@users.noreply.github.com>
2023-07-02 00:00:12 +0900

    Update 2477 참외밭.py

--- 00. 백준 문제/(2) 2000_3000 백준 문제/2477 참외밭.py
+++ 00. 백준 문제/(2) 2000_3000 백준 문제/2477 참외밭.py
@@ -1,41 +1,16 @@
-s = [] # 방향, 거리 저장 리스트
-x = [] # 가로 길이들 리스트
-y = [] # 세로 길이들 리스트
-lownum = [] # B의 가로 세로 길이
- 
-k = int(input())
- 
-for i in range(6):
-    way,dist = map(int,input().split()) # 방향, 거리 입력
-    s.append([way,dist])
-    if s[i][0] == 3 or s[i][0] == 4: # 세로 저장
-        x.append(s[i][1])
-    if s[i][0] == 1 or s[i][0] == 2: # 가로 저장
-        y.append(s[i][1])
- 
-# B의 길이 추출
-for i in range(6):
-    if s[i][0] == s[(i+2)%6][0]:
-        lownum.append(s[(i+1)%6][1])
-        
-print( ((max(x)*max(y)) - (lownum[0]*lownum[1])) * k )
- 
+melon = int(input()) # 참외 개수 K
+values = [input().split() for _ in range(6)] # 나머지 2~7 line의 6 줄을 입력 받는다.
+directions = [int(v[0]) for v in values] # 방향을 뽑아내서 저장한다.
+lengths = [int(v[1]) for v in values] # 길이를 뽑아내서 저장한다.
+max_lengths, box_lengths = [], [] # 큰 박스의 길이, 작은 박스의 길이를 담을 배열
 
+for i in range(1, 5):
+    if directions.count(i) == 1: # direction이 한번만 존재한다 == 큰 박스의 변
+        max_lengths.append(lengths[directions.index(i)]) # 큰박스의 변 길이 저장
+        temp = directions.index(i) + 3 # 큰 박스 + 3 == 작은 박스의 변
+        if temp >= 6:
+            temp -= 6 # cycle을 위해 6 이상일 경우 -6
+        box_lengths.append(lengths[temp]) 
 
-공감
-
-'Problem Solving > CT-Python' 카테고리의 다른 글
-[백준/DP] 16395: 파스칼의 삼각형 = [SWEA] 2005: 파스칼의 삼각형 - 파이썬  (0)	2022.04.25
-[백준/브루트포스] 17614: 369 - 파이썬  (0)	2022.04.25
-[SWEA] 1206: [S/W 문제해결 기본] 1일차 - View - 파이썬  (0)	2022.04.22
-[백준/브루트포스] 2635: 수 이어가기 - 파이썬  (0)	2022.04.22
-[백준/구현] 2669: 직사각형 네개의 합집합의 면적 구하기 - 파이썬  (0)	2022.04.22
-'Problem Solving/CT-Python' 카테고리의 다른 글
-[백준/DP] 16395: 파스칼의 삼각형 = [SWEA] 2005: 파스칼의 삼각형 - 파이썬
-[백준/브루트포스] 17614: 369 - 파이썬
-[SWEA] 1206: [S/W 문제해결 기본] 1일차 - View - 파이썬
-[백준/브루트포스] 2635: 수 이어가기 - 파이썬
-
-White Asher
-
-이름 암호댓글쓰기
+area = max_lengths[0] * max_lengths[1] - box_lengths[0] * box_lengths[1]
+print(melon * area)
